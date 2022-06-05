b991985c18ade63a3fce0113609f0f399d676225
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2022-06-05 13:55:03 -0400

    insertar datos al abrir localhost:9000 en tabla users

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose');
 require("dotenv").config();
-
 const app = express();
 const port = process.env.PORT || 9000;//process.env.port es propio dejavascript
 
+const UserModel = require("./models/User");
+//middlewares
+
+
 //routes
-app.get('/',(req,res) =>{
+app.get('/', async (req,res) =>{
+    const user = new UserModel({name:"zapato",password:"123",email:"zapato@asd.com"});
+    try{
+        await user.save();
+    }catch(err){
+        console.log(err)
+    }
     res.send('welcome To my API')
 })
 //mongodb connection
 mongoose
     .connect(process.env.MONGODB_URI)
-    .then(()=>console.log("connected to mongodb"))
-    .catch((error)=>console.error(error)) 
+    .then(()=>console.log("connected to mongodb"))//recordar encender la bd
+    .catch((error)=>console.error(error)) //con las instrucciones del escritorio
 
 app.listen(9000, () => console.log('server listening on port', 9000));
